10de7a6aa29ee7088cb732034d983092f852aa23
ilya <zakharuni13@gmail.com>
2015-01-26 14:03:15 +0300

    cleaning in controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,45 +1,52 @@
 class ProjectsController < ApplicationController	
-	before_action :authenticate_admin!, except: [:index, :show]
-	before_action :get_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: [:index, :show]
+  before_action :get_project, only: [:show, :edit, :update, :destroy]
 
-	def get_project
-		@project = Project.find(params[:id])
-	end
-	def index
-		@projects = Project.all		
-	end
-	def show
-		redirect_to project_tasks_path(@project)			
-	end
-	def new
-		@project = Project.new
-	end
-	def edit
-		
-	end
-	def create
-		@project = Project.new(project_params)
+  def get_project
+	@project = Project.find(params[:id])
+  end
 
-		if @project.save
-		  redirect_to projects_path
-		else			
-			render 'new'
-		end
-	end
-	def update	    
-	    if(@project.update_attributes(project_params))
-	    	redirect_to projects_path
-	    else
-	    	render 'edit'
-	    end	   
-	end
-	def destroy		
-		@project.destroy
+  def index
+	@projects = Project.all		
+  end
 
-		redirect_to projects_path
+  def show
+	redirect_to project_tasks_path(@project)			
+  end
+
+  def new
+	@project = Project.new
+  end
+
+  def edit
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+	if @project.save
+	  redirect_to projects_path
+	else			
+	  render 'new'
 	end
-	private
-	  def project_params
-	     params.require(:project).permit(:title)
-	  end		
+  end
+
+  def update	    
+    if(@project.update_attributes(project_params))
+      redirect_to projects_path
+	else
+	  render 'edit'
+	end	   
+  end
+
+  def destroy		
+	@project.destroy
+
+	redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title)
+	end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,62 +1,60 @@
 class TasksController < ApplicationController
-	before_action :authenticate_admin!, except: [:index, :show]
-	before_action :get_project
-	before_action :get_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: [:index, :show]
+  before_action :get_project
+  before_action :get_task, only: [:show, :edit, :update, :destroy]
 
-	def get_project
-		@project = Project.find(params[:project_id])
+  def get_project
+	@project = Project.find(params[:project_id])
+  end
+
+  def get_task
+	@task = Task.find(params[:id])
+  end
+
+  def index	    	    		
+	@tasks = @project.tasks
+  end
+
+  def show
+  end
+
+  def new		
+	@task = Task.new
+  end
+
+  def edit
+  end
+
+  def create		
+	@task = @project.tasks.new(task_params)
+
+	if @task.save
+	  redirect_to project_tasks_path(@project)
+	else			
+      render 'new'
 	end
+  end
 
-	def get_task
-		@task = Task.find(params[:id])
-	end
-
-	def index	    	    		
-		@tasks = @project.tasks
-	end
-
-	def show		 
-					
-	end
-
-	def new		
-		@task = Task.new
-	end
-
-	def edit
-		
-	end
-
-	def create		
-		@task = @project.tasks.new(task_params)
-
-		if @task.save
-		  redirect_to project_tasks_path(@project)
-		else			
-			render 'new'
-		end
-	end
-
-	def update	
+  def update	
 	   
-	    if(@task.update_attributes(task_params))
-	    	redirect_to project_tasks_path(@project)
-	    else
-	    	render 'edit'
-	    end	    
-	end
+    if(@task.update_attributes(task_params))
+      redirect_to project_tasks_path(@project)
+    else
+   	  render 'edit'
+	end	    
+  end
 
-	def destroy
+  def destroy
 
-		@task.destroy
+	@task.destroy
 
-		redirect_to project_tasks_path(@project)
-	end
+	redirect_to project_tasks_path(@project)
+  end
 
-	private
-	  def task_params
-	     params.require(:task).permit(:title,
-	     :rating, :description, :customer, :performer,
-	     :project_id, :state)
-	  end		
+  private
+    def task_params
+      params.require(:task).permit(:title,
+      :rating, :description, :customer, :performer,
+      :project_id, :state)
+    end		
 end
